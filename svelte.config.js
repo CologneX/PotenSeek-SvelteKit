617ab4b38a1b5bda42e958a8d1d7e0f26790bd69
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-05-10 02:59:51 +0700

    (svelte.config) fixed error code

--- svelte.config.js
+++ svelte.config.js
@@ -13,11 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 
 		adapter: adapter(),
-		vite: {
-			optimizeDeps: {
-				include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-			}
-		}
+		
 	}
 };
 
